6e0e15d710c82fcba727c14e8b57195dfe2dd4aa
YI ZHANG <scaccterran@gmail.com>
2020-12-20 21:35:14 -0500

    POM-4 setup code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     preset: '@vue/cli-plugin-unit-jest',
-    transform: {
-        '^.+\\.vue$': 'vue-jest'
-    }
+    transform: { '^.+\\.vue$': 'vue-jest' },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**src/**/*.{js,vue}',
+        '!**/node_modules/**'
+    ]
 }
