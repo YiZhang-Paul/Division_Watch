53f0e8e50cc0b0398fb343f001d61fcd0576a05d
YI ZHANG <scaccterran@gmail.com>
2020-12-24 18:42:41 -0500

    POM-36 added base for access menu

--- src/features/agent-watch/AgentWatch.vue
+++ src/features/agent-watch/AgentWatch.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="agent-watch-container">
         <watch-base></watch-base>
-        <battery-display class="battery-display"></battery-display>
-        <weather-display class="weather-display"></weather-display>
-        <time-display class="time-display"></time-display>
-        <img class="logo" src="../../assets/images/shd_tech.jpg" draggable="false" />
+
+        <template v-if="!isMenuOn">
+            <battery-display class="battery-display"></battery-display>
+            <weather-display class="weather-display"></weather-display>
+            <time-display class="time-display"></time-display>
+            <img class="logo" @click="openMenu()" src="../../assets/images/shd_tech.jpg" draggable="false" />
+        </template>
+
+        <div v-if="isMenuOn" class="menu-overlay">
+            <div class="option-name"></div>
+        </div>
     </div>
 </template>
 
@@ -24,7 +31,13 @@ import WatchBase from '../../shared/WatchBase.vue';
         WatchBase
     }
 })
-export default class AgentWatch extends Vue { }
+export default class AgentWatch extends Vue {
+    public isMenuOn = false;
+
+    public openMenu(): void {
+        this.isMenuOn = true;
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -68,6 +81,41 @@ export default class AgentWatch extends Vue { }
         top: 5%;
         right: calc(50% - #{$width} / 2);
         width: $width;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    .menu-overlay {
+        $dimension: 76%;
+
+        position: absolute;
+        top: calc(50% - #{$dimension} / 2);
+        left: calc(50% - #{$dimension} / 2);
+        width: calc(#{$dimension} - 4px);
+        height: calc(#{$dimension} - 4px);
+        background: linear-gradient(to bottom, rgba(121, 117, 131, 0.33), rgba(54, 53, 103, 0.33));
+        background-color: rgba(49, 48, 54, 0.85);
+        border: 2px solid rgba(218, 220, 69, 0.9);
+        border-radius: 50%;
+        opacity: 0.95;
+
+        .option-name {
+            $dimension: 60%;
+
+            position: absolute;
+            top: calc(50% - #{$dimension} / 2);
+            left: calc(50% - #{$dimension} / 2);
+            width: calc(#{$dimension} - 4px);
+            height: calc(#{$dimension} - 4px);
+            background: linear-gradient(to bottom, rgba(121, 117, 131, 0.33), rgba(54, 53, 103, 0.33));
+            background-color: rgba(49, 48, 54, 0.85);
+            border: 2px solid rgba(218, 220, 69, 0.9);
+            border-radius: 50%;
+            box-shadow: 0 0 4px 1px rgba(227, 94, 19, 0.75);
+            opacity: 0.95;
+        }
     }
 }
 </style>
