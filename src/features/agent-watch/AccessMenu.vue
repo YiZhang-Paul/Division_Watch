df2538e9e23e1b47e9539aaddf1f9807ff4fc2a4
YI ZHANG <scaccterran@gmail.com>
2020-12-25 19:37:53 -0500

    POM-36 extracted access menu to separate component

---
+++ src/features/agent-watch/AccessMenu.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="access-menu-container glass-panel">
+        <div v-for="option of options"
+            class="option-button"
+            :style="{ transform: 'rotate(' + (isOptionsVisible ? option.angle : 0) + 'deg)' }"
+            :key="option.name">
+
+            <button class="glass-panel"
+                :style="{ transform: 'rotate(' + -option.angle + 'deg)', color: isOptionsDisabled ? 'grey' : option.color }"
+                @mouseover="activeOption = option.name"
+                @mouseout="activeOption = ''"
+                @click="$emit('menu:select', option.name)">
+
+                <power-standby v-if="option.name === 'On/Off'" />
+                <play v-if="option.name === 'Start'" />
+                <timer-sand v-if="option.name === 'Tasks'" />
+                <palette-swatch v-if="option.name === 'Planner'" />
+                <inbox-multiple v-if="option.name === 'Backlog'" />
+                <finance v-if="option.name === 'Stats'" />
+                <cog v-if="option.name === 'Settings'" />
+            </button>
+        </div>
+
+        <div class="option-name glass-panel">
+            <span v-if="activeOption">{{ activeOption }}</span>
+
+            <close-circle v-if="!activeOption"
+                class="close-menu"
+                @mouseover="isOptionsDisabled = true"
+                @mouseout="isOptionsDisabled = false"
+                @click="$emit('menu:close')" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { Cog, CloseCircle, Finance, InboxMultiple, PowerStandby, Play, PaletteSwatch, TimerSand } from 'mdue';
+
+@Options({
+    components: {
+        Cog,
+        CloseCircle,
+        Finance,
+        InboxMultiple,
+        PowerStandby,
+        Play,
+        PaletteSwatch,
+        TimerSand,
+    },
+    emits: [
+        'menu:select',
+        'menu:close'
+    ]
+})
+export default class AccessMenu extends Vue {
+    public options = [
+        { name: 'On/Off', angle: 51, color: 'rgb(24, 238, 20)' },
+        { name: 'Start', angle: 131, color: 'rgb(24, 238, 20)' },
+        { name: 'Tasks', angle: 211, color: 'rgb(238, 255, 133)' },
+        { name: 'Planner', angle: 251, color: 'rgb(238, 123, 107)' },
+        { name: 'Backlog', angle: 291, color: 'rgb(255, 9, 9)' },
+        { name: 'Stats', angle: 331, color: 'rgb(33, 188, 254)' },
+        { name: 'Settings', angle: 371, color: 'rgb(255, 255, 255)' }
+    ];
+
+    public activeOption = '';
+    public isOptionsVisible = false;
+    public isOptionsDisabled = false;
+
+    public mounted(): void {
+        setTimeout(() => this.isOptionsVisible = true, 50);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.glass-panel {
+    background: linear-gradient(to bottom, rgba(121, 117, 131, 0.33), rgba(54, 53, 103, 0.33));
+    background-color: rgba(18, 18, 19, 0.95);
+    box-shadow: 0 0 4px 1px rgba(227, 94, 19, 0.75);
+    border: 2px solid rgba(218, 220, 69, 0.9);
+    border-radius: 50%;
+    opacity: 0.95;
+}
+
+.access-menu-container {
+    $option-name-dimension: 60%;
+
+    width: 100%;
+    height: 100%;
+    color: rgb(255, 255, 255);
+    font-family: 'Bruno Ace';
+
+    .option-name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: calc(50% - #{$option-name-dimension} / 2);
+        left: calc(50% - #{$option-name-dimension} / 2);
+        width: calc(#{$option-name-dimension} - 4px);
+        height: calc(#{$option-name-dimension} - 4px);
+        font-size: 2em;
+    }
+
+    .option-button {
+        position: absolute;
+        top: 49.5%;
+        left: 0;
+        width: 50%;
+        height: 1%;
+        transform-origin: 100% 50%;
+        transition: transform 0.5s;
+    }
+
+    .close-menu {
+        color: rgb(255, 0, 0);
+        font-size: 2.5em;
+        transition: color 0.15s, font-size 0.15s;
+
+        &:hover {
+            cursor: pointer;
+            color: rgb(240, 44, 44);
+            font-size: 3em;
+        }
+    }
+
+    button {
+        $gap: 1%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        bottom: calc((50% - #{$option-name-dimension} / 2) * -50);
+        left: $gap;
+        width: calc((50% - #{$option-name-dimension} / 2 - #{$gap}) * 2);
+        height: calc((50% - #{$option-name-dimension} / 2 - #{$gap}) * 100);
+        outline: none;
+        font-size: 2.5em;
+        transition: border 0.3s, box-shadow 0.3s, color 0.15s;
+
+        &:hover {
+            cursor: pointer;
+            box-shadow: 0 0 12px 2px rgba(227, 94, 19, 0.9);
+        }
+
+        &:active {
+            background-color: rgba(18, 18, 19, 0.25);
+        }
+    }
+}
+</style>
--- src/features/agent-watch/AgentWatch.vue
+++ src/features/agent-watch/AgentWatch.vue
@@ -4,96 +4,32 @@
         <battery-display v-show="!isMenuOn" class="battery-display"></battery-display>
         <weather-display v-show="!isMenuOn" class="weather-display"></weather-display>
         <time-display v-show="!isMenuOn" class="time-display"></time-display>
-        <img v-show="!isMenuOn" class="logo" @click="openMenu()" src="../../assets/images/shd_tech.jpg" draggable="false" />
-
-        <div v-if="isMenuOn" class="menu-overlay glass-panel">
-            <div v-for="option of options"
-                class="option-button"
-                :style="{ transform: 'rotate(' + (isOptionsVisible ? option.angle : 0) + 'deg)' }"
-                :key="option.name">
-
-                <button class="glass-panel"
-                    :style="{ transform: 'rotate(' + -option.angle + 'deg)', color: isOptionsDisabled ? 'grey' : option.color }"
-                    @mouseover="activeOption = option.name"
-                    @mouseout="activeOption = ''"
-                    @click="$emit('menu:select', option.name)">
-
-                    <power-standby v-if="option.name === 'On/Off'" />
-                    <play v-if="option.name === 'Start'" />
-                    <timer-sand v-if="option.name === 'Tasks'" />
-                    <palette-swatch v-if="option.name === 'Planner'" />
-                    <inbox-multiple v-if="option.name === 'Backlog'" />
-                    <finance v-if="option.name === 'Stats'" />
-                    <cog v-if="option.name === 'Settings'" />
-                </button>
-            </div>
-
-            <div class="option-name glass-panel">
-                <span v-if="activeOption">{{ activeOption }}</span>
-
-                <close-circle v-if="!activeOption"
-                    class="close-menu"
-                    @mouseover="isOptionsDisabled = true"
-                    @mouseout="isOptionsDisabled = false"
-                    @click="closeMenu()" />
-            </div>
-        </div>
+        <img v-show="!isMenuOn" class="logo" @click="isMenuOn = true" src="../../assets/images/shd_tech.jpg" draggable="false" />
+        <access-menu v-if="isMenuOn" class="access-menu" @menu:close="isMenuOn = false"></access-menu>
     </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import { Cog, CloseCircle, Finance, InboxMultiple, PowerStandby, Play, PaletteSwatch, TimerSand } from 'mdue';
 
 import BatteryDisplay from '../../shared/BatteryDisplay.vue';
 import WeatherDisplay from '../../shared/WeatherDisplay.vue';
 import TimeDisplay from '../../shared/TimeDisplay.vue';
 import WatchBase from '../../shared/WatchBase.vue';
 
+import AccessMenu from './AccessMenu.vue';
+
 @Options({
     components: {
-        Cog,
-        CloseCircle,
-        Finance,
-        InboxMultiple,
-        PowerStandby,
-        Play,
-        PaletteSwatch,
-        TimerSand,
+        AccessMenu,
         BatteryDisplay,
         WeatherDisplay,
         TimeDisplay,
         WatchBase
-    },
-    emits: ['menu:select']
+    }
 })
 export default class AgentWatch extends Vue {
-    public options = [
-        { name: 'On/Off', angle: 51, color: 'rgb(24, 238, 20)' },
-        { name: 'Start', angle: 131, color: 'rgb(24, 238, 20)' },
-        { name: 'Tasks', angle: 211, color: 'rgb(238, 255, 133)' },
-        { name: 'Planner', angle: 251, color: 'rgb(238, 123, 107)' },
-        { name: 'Backlog', angle: 291, color: 'rgb(255, 9, 9)' },
-        { name: 'Stats', angle: 331, color: 'rgb(33, 188, 254)' },
-        { name: 'Settings', angle: 371, color: 'rgb(255, 255, 255)' }
-    ];
-
-    public activeOption = '';
     public isMenuOn = false;
-    public isOptionsVisible = false;
-    public isOptionsDisabled = false;
-
-    public openMenu(): void {
-        this.isMenuOn = true;
-        this.isOptionsDisabled = false;
-        setTimeout(() => this.isOptionsVisible = true, 50);
-    }
-
-    public closeMenu(): void {
-        this.isMenuOn = false;
-        this.isOptionsVisible = false;
-        this.isOptionsDisabled = false;
-    }
 }
 </script>
 
@@ -144,83 +80,14 @@ export default class AgentWatch extends Vue {
         }
     }
 
-    .glass-panel {
-        background: linear-gradient(to bottom, rgba(121, 117, 131, 0.33), rgba(54, 53, 103, 0.33));
-        background-color: rgba(18, 18, 19, 0.95);
-        box-shadow: 0 0 4px 1px rgba(227, 94, 19, 0.75);
-        border: 2px solid rgba(218, 220, 69, 0.9);
-        border-radius: 50%;
-        opacity: 0.95;
-    }
-
-    .menu-overlay {
+    .access-menu {
         $overlay-dimension: 76%;
-        $option-name-dimension: 60%;
 
         position: absolute;
         top: calc(50% - #{$overlay-dimension} / 2);
         left: calc(50% - #{$overlay-dimension} / 2);
         width: calc(#{$overlay-dimension} - 4px);
         height: calc(#{$overlay-dimension} - 4px);
-        color: rgb(255, 255, 255);
-        font-family: 'Bruno Ace';
-
-        .close-menu {
-            font-size: 2.5em;
-            color: rgb(255, 0, 0);
-            transition: color 0.15s, font-size 0.15s;
-
-            &:hover {
-                cursor: pointer;
-                font-size: 3em;
-                color: rgb(240, 44, 44);
-            }
-        }
-
-        .option-name {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            top: calc(50% - #{$option-name-dimension} / 2);
-            left: calc(50% - #{$option-name-dimension} / 2);
-            width: calc(#{$option-name-dimension} - 4px);
-            height: calc(#{$option-name-dimension} - 4px);
-            font-size: 2em;
-        }
-
-        .option-button {
-            position: absolute;
-            top: 49.5%;
-            left: 0;
-            width: 50%;
-            height: 1%;
-            transform-origin: 100% 50%;
-            transition: transform 0.5s;
-        }
-
-        button {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            bottom: calc((#{$overlay-dimension} - #{$option-name-dimension}) * -50);
-            left: 4%;
-            width: calc((#{$overlay-dimension} - #{$option-name-dimension}) * 2);
-            height: calc((#{$overlay-dimension} - #{$option-name-dimension}) * 100);
-            outline: none;
-            font-size: 2.5em;
-            transition: border 0.3s, box-shadow 0.3s, color 0.15s;
-
-            &:hover {
-                cursor: pointer;
-                box-shadow: 0 0 12px 2px rgba(227, 94, 19, 0.9);
-            }
-
-            &:active {
-                background-color: rgba(18, 18, 19, 0.25);
-            }
-        }
     }
 }
 </style>
