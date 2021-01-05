94bef5e771c25b73112720f13fc67aafedfb49a2
YI ZHANG <scaccterran@gmail.com>
2021-01-04 20:55:31 -0500

    POM-14 render task summary in lists

--- src/shared/components/TaskList.vue
+++ src/shared/components/TaskList.vue
@@ -1,37 +1,81 @@
 <template>
     <glass-panel class="task-list-container">
-        <input-panel class="task-node">
-            <div class="node-content">
-                <timer-sand class="icon" />
-                <span>Tasks</span>
-                <plus class="add-button" />
+        <div class="task-list-content">
+            <div class="active-indicator" v-if="activeNode === 'task'">
+                <div></div>
             </div>
-        </input-panel>
 
-        <input-panel class="interruption-node">
-            <div class="node-content">
-                <exclamation-thick class="icon" />
-                <span>Interruptions</span>
-                <plus class="add-button" />
-            </div>
-        </input-panel>
+            <input-panel class="task-node" @click="activeNode = 'task'">
+                <div class="node-content" :class="{ 'active-node': activeNode === 'task' }">
+                    <timer-sand class="icon" />
+                    <span>Tasks</span>
+                    <span>x{{ tasks.length }}</span>
+                    <plus class="add-button" />
+                </div>
+            </input-panel>
 
-        <input-panel class="category-node">
-            <div class="node-content">
-                <format-list-bulleted-type class="icon" />
-                <span>Categories</span>
-                <plus class="add-button" />
+            <div class="task-node-items" :class="{ 'active-list': activeNode === 'task' }">
+                <div v-for="task of tasks"
+                    :key="task.name"
+                    class="task-summary"
+                    :style="{ 'background-color': getSummaryColor(task, true) }">
+
+                    <div :style="{ 'background-color': getSummaryColor(task) }"></div>
+                    <span>{{ task.name }}</span>
+                </div>
             </div>
-        </input-panel>
+
+            <div class="active-indicator" v-if="activeNode === 'interruption'">
+                <div></div>
+            </div>
+
+            <input-panel class="interruption-node" @click="activeNode = 'interruption'">
+                <div class="node-content" :class="{ 'active-node': activeNode === 'interruption' }">
+                    <exclamation-thick class="icon" />
+                    <span>Interruptions</span>
+                    <span>x{{ interruptions.length }}</span>
+                    <plus class="add-button" />
+                </div>
+            </input-panel>
+
+            <div class="interruption-node-items" :class="{ 'active-list': activeNode === 'interruption' }">
+                <div v-for="interruption of interruptions"
+                    :key="interruption.name"
+                    class="task-summary"
+                    :style="{ 'background-color': getSummaryColor(interruption, true) }">
+
+                    <div :style="{ 'background-color': getSummaryColor(interruption) }"></div>
+                    <span>{{ interruption.name }}</span>
+                </div>
+            </div>
+
+            <div class="active-indicator" v-if="activeNode === 'category'">
+                <div></div>
+            </div>
+
+            <input-panel class="category-node" @click="activeNode = 'category'">
+                <div class="node-content" :class="{ 'active-node': activeNode === 'category' }">
+                    <format-list-bulleted-type class="icon" />
+                    <span>Categories</span>
+                    <plus class="add-button" />
+                </div>
+            </input-panel>
+
+            <div class="category-node-items" :class="{ 'active-list': activeNode === 'category' }"></div>
+        </div>
     </glass-panel>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { ExclamationThick, FormatListBulletedType, Plus, TimerSand } from 'mdue';
-
+// eslint-disable-next-line no-unused-vars
+import { TaskItem } from '../../core/data-model/task-item';
+import { TaskItemHttpService } from '../../core/services/http/task-item-http/task-item-http.service';
 import InputPanel from '../../shared/panels/InputPanel.vue';
 import GlassPanel from '../../shared/panels/GlassPanel.vue';
+
+const taskItemHttpService = new TaskItemHttpService();
 
 @Options({
     components: {
@@ -43,11 +87,102 @@ import GlassPanel from '../../shared/panels/GlassPanel.vue';
         GlassPanel
     }
 })
-export default class TaskList extends Vue { }
+export default class TaskList extends Vue {
+    public activeNode = 'task';
+    public tasks: TaskItem[] = [];
+    public interruptions: TaskItem[] = [];
+
+    public async created(): Promise<void> {
+        const items = await taskItemHttpService.getTaskItems();
+        this.tasks = items.filter(_ => !_.isInterruption);
+        this.interruptions = items.filter(_ => _.isInterruption);
+    }
+
+    public getSummaryColor(item: TaskItem, isAlpha = false): string {
+        const prefix = isAlpha ? 'rgba(' : 'rgb(';
+        const suffix = isAlpha ? ', 0.5)' : ')';
+
+        if (!item.priority) {
+            return `${prefix}238, 171, 70${suffix}`;
+        }
+
+        return `${prefix}${item.priority === 1 ? '231, 72, 72' : '240, 113, 40'}${suffix}`;
+    }
+}
 </script>
 
 <style lang="scss" scoped>
 .task-list-container {
+
+    .active-indicator {
+        position: relative;
+        margin-top: 0;
+        width: 100%;
+        height: 0;
+        animation: expandIndicatorVertical 0.2s ease forwards;
+
+        div {
+            position: absolute;
+            left: 42.5%;
+            width: 15%;
+            height: 100%;
+            background-color: rgb(246, 149, 78);
+            filter: brightness(1);
+            opacity: 0;
+            animation: expandIndicatorHorizontal 0.25s ease 0.1s forwards;
+        }
+    }
+
+    .task-list-content {
+        display: flex;
+        flex-direction: column;
+        padding-top: 5px;
+        width: 100%;
+        height: calc(100% - 5px);
+    }
+
+    .task-node-items,
+    .interruption-node-items,
+    .category-node-items {
+        width: 100%;
+        flex: 0;
+        overflow-y: auto;
+        transition: flex 0.4s;
+    }
+
+    .task-summary {
+        display: flex;
+        align-items: center;
+        margin-bottom: 1px;
+        width: 100%;
+        height: 8%;
+        max-height: 45px;
+        color: rgba(255, 255, 255, 0.7);
+        font-family: 'Segoe UI';
+        font-size: 0.9em;
+        opacity: 0;
+        transition: opacity 0.15s, color 0.3s;
+
+        div:first-of-type {
+            margin-right: 5%;
+            width: 4%;
+            height: 100%;
+        }
+
+        &:hover {
+            cursor: pointer;
+            color: rgba(255, 255, 255);
+            filter: brightness(1.2);
+        }
+    }
+
+    .active-list {
+        flex: 1;
+
+        .task-summary {
+            animation: revealSummary 0.3s ease 0.4s forwards;
+        }
+    }
 
     .task-node,
     .interruption-node,
@@ -69,7 +204,7 @@ export default class TaskList extends Vue { }
             font-size: 1.25em;
             transition: color 0.3s, filter 0.3s, background-color 0.2s;
 
-            span:first-of-type {
+            span {
                 margin-left: 0.5em;
             }
         }
@@ -83,9 +218,18 @@ export default class TaskList extends Vue { }
             margin-left: auto;
             color: rgb(246, 149, 78);
             opacity: 0;
-            transition: filter 0.3s;
+            transition: filter 0.3s, opacity 0.3s;
 
             &:hover {
+                filter: brightness(1);
+            }
+        }
+
+        .active-node {
+            background-color: rgb(63, 62, 68);
+            color: rgb(255, 255, 255);
+
+            .icon {
                 filter: brightness(1);
             }
         }
@@ -108,12 +252,8 @@ export default class TaskList extends Vue { }
         }
     }
 
-    .task-node {
-        margin-top: 5px;
-
-        .icon {
-            color: rgb(255, 28, 82);
-        }
+    .task-node .icon {
+        color: rgb(255, 28, 82);
     }
 
     .interruption-node .icon {
@@ -122,6 +262,42 @@ export default class TaskList extends Vue { }
 
     .category-node .icon {
         color: rgb(245, 238, 58);
+    }
+
+    @keyframes expandIndicatorVertical {
+        from {
+            margin-top: 0;
+            height: 0;
+        }
+        to {
+            margin-top: 2px;
+            height: 0.25em;
+        }
+    }
+
+    @keyframes expandIndicatorHorizontal {
+        0% {
+            left: 42.5%;
+            width: 15%;
+            opacity: 0;
+        }
+        1% {
+            opacity: 1;
+        }
+        100% {
+            left: 0;
+            width: 100%;
+            opacity: 1;
+        }
+    }
+
+    @keyframes revealSummary {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
     }
 }
 </style>
