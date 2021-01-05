224e6ad94b778bc11c1921dc1c0e1d3440edce83
YI ZHANG <scaccterran@gmail.com>
2021-01-05 07:47:23 -0500

    POM-14 task summary card component

---
+++ src/shared/cards/TaskSummaryCard.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="task" class="task-summary-card-container" :style="containerStyle">
+        <div :style="indicatorStyle"></div>
+        <span>{{ task.name }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, prop } from 'vue-class-component';
+// eslint-disable-next-line no-unused-vars
+import { TaskItem } from '../../core/data-model/task-item';
+
+class TaskSummaryCardProp {
+    public task = prop<TaskItem>({ default: null });
+}
+
+export default class TaskSummaryCard extends Vue.with(TaskSummaryCardProp) {
+
+    get containerStyle(): { [key: string]: string } {
+        return { 'background-color': `rgba(${this.baseColor}, 0.5)` };
+    }
+
+    get indicatorStyle(): { [key: string]: string } {
+        return { 'background-color': `rgb(${this.baseColor})` };
+    }
+
+    get baseColor(): string {
+        if (!this.task.priority) {
+            return '238, 171, 70';
+        }
+
+        return this.task.priority === 1 ? '231, 72, 72' : '240, 113, 40';
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.task-summary-card-container {
+    display: flex;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.7);
+    font-family: 'Segoe UI';
+    font-size: 0.9em;
+    transition: opacity 0.15s, color 0.3s;
+
+    div:first-of-type {
+        margin-right: 5%;
+        width: 4%;
+        height: 100%;
+    }
+
+    &:hover {
+        cursor: pointer;
+        color: rgba(255, 255, 255);
+        filter: brightness(1.2);
+    }
+}
+</style>
--- src/shared/components/TaskList.vue
+++ src/shared/components/TaskList.vue
@@ -15,14 +15,11 @@
             </input-panel>
 
             <div class="task-node-items" :class="{ 'active-list': activeNode === 'task' }">
-                <div v-for="task of tasks"
+                <task-summary-card v-for="task of tasks"
+                    class="summary-card"
                     :key="task.name"
-                    class="task-summary"
-                    :style="{ 'background-color': getSummaryColor(task, true) }">
-
-                    <div :style="{ 'background-color': getSummaryColor(task) }"></div>
-                    <span>{{ task.name }}</span>
-                </div>
+                    :task="task">
+                </task-summary-card>
             </div>
 
             <div class="active-indicator" v-if="activeNode === 'interruption'">
@@ -39,14 +36,11 @@
             </input-panel>
 
             <div class="interruption-node-items" :class="{ 'active-list': activeNode === 'interruption' }">
-                <div v-for="interruption of interruptions"
+                <task-summary-card v-for="interruption of interruptions"
+                    class="summary-card"
                     :key="interruption.name"
-                    class="task-summary"
-                    :style="{ 'background-color': getSummaryColor(interruption, true) }">
-
-                    <div :style="{ 'background-color': getSummaryColor(interruption) }"></div>
-                    <span>{{ interruption.name }}</span>
-                </div>
+                    :task="interruption">
+                </task-summary-card>
             </div>
 
             <div class="active-indicator" v-if="activeNode === 'category'">
@@ -72,6 +66,7 @@ import { ExclamationThick, FormatListBulletedType, Plus, TimerSand } from 'mdue'
 // eslint-disable-next-line no-unused-vars
 import { TaskItem } from '../../core/data-model/task-item';
 import { TaskItemHttpService } from '../../core/services/http/task-item-http/task-item-http.service';
+import TaskSummaryCard from '../../shared/cards/TaskSummaryCard.vue';
 import InputPanel from '../../shared/panels/InputPanel.vue';
 import GlassPanel from '../../shared/panels/GlassPanel.vue';
 
@@ -83,6 +78,7 @@ const taskItemHttpService = new TaskItemHttpService();
         FormatListBulletedType,
         Plus,
         TimerSand,
+        TaskSummaryCard,
         InputPanel,
         GlassPanel
     }
@@ -96,17 +92,6 @@ export default class TaskList extends Vue {
         const items = await taskItemHttpService.getTaskItems();
         this.tasks = items.filter(_ => !_.isInterruption);
         this.interruptions = items.filter(_ => _.isInterruption);
-    }
-
-    public getSummaryColor(item: TaskItem, isAlpha = false): string {
-        const prefix = isAlpha ? 'rgba(' : 'rgb(';
-        const suffix = isAlpha ? ', 0.5)' : ')';
-
-        if (!item.priority) {
-            return `${prefix}238, 171, 70${suffix}`;
-        }
-
-        return `${prefix}${item.priority === 1 ? '231, 72, 72' : '240, 113, 40'}${suffix}`;
     }
 }
 </script>
@@ -150,36 +135,18 @@ export default class TaskList extends Vue {
         transition: flex 0.4s;
     }
 
-    .task-summary {
-        display: flex;
-        align-items: center;
-        margin-bottom: 1px;
+    .summary-card {
         width: 100%;
         height: 8%;
         max-height: 45px;
-        color: rgba(255, 255, 255, 0.7);
-        font-family: 'Segoe UI';
-        font-size: 0.9em;
+        margin-bottom: 1px;
         opacity: 0;
-        transition: opacity 0.15s, color 0.3s;
-
-        div:first-of-type {
-            margin-right: 5%;
-            width: 4%;
-            height: 100%;
-        }
-
-        &:hover {
-            cursor: pointer;
-            color: rgba(255, 255, 255);
-            filter: brightness(1.2);
-        }
     }
 
     .active-list {
         flex: 1;
 
-        .task-summary {
+        .summary-card {
             animation: revealSummary 0.3s ease 0.4s forwards;
         }
     }
