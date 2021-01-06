395a8b1cd5249ecf8511fc251f530f88f79639bd
YI ZHANG <scaccterran@gmail.com>
2021-01-05 20:05:43 -0500

    POM-14 task list component

--- src/features/task-manager/TaskSelector.vue
+++ src/features/task-manager/TaskSelector.vue
@@ -1,90 +1,58 @@
 <template>
     <glass-panel class="task-selector-container">
         <div class="task-selector-content">
-            <div class="active-indicator" v-if="activeNode === 'task'">
-                <div></div>
-            </div>
+            <task-list class="task-list"
+                :isActive="activeButton === taskButton"
+                :action="taskButton"
+                :tasks="tasks"
+                @activate="activeButton = taskButton">
+            </task-list>
 
-            <input-panel class="task-node" @click="activeNode = 'task'">
-                <div class="node-content" :class="{ 'active-node': activeNode === 'task' }">
-                    <timer-sand class="icon" />
-                    <span>Tasks</span>
-                    <span>x{{ tasks.length }}</span>
-                    <plus class="add-button" />
-                </div>
-            </input-panel>
+            <task-list class="task-list"
+                :isActive="activeButton === interruptionButton"
+                :action="interruptionButton"
+                :tasks="interruptions"
+                @activate="activeButton = interruptionButton">
+            </task-list>
 
-            <div class="task-node-items" :class="{ 'active-list': activeNode === 'task' }">
-                <task-summary-card v-for="task of tasks"
-                    class="summary-card"
-                    :key="task.name"
-                    :task="task">
-                </task-summary-card>
-            </div>
-
-            <div class="active-indicator" v-if="activeNode === 'interruption'">
-                <div></div>
-            </div>
-
-            <input-panel class="interruption-node" @click="activeNode = 'interruption'">
-                <div class="node-content" :class="{ 'active-node': activeNode === 'interruption' }">
-                    <exclamation-thick class="icon" />
-                    <span>Interruptions</span>
-                    <span>x{{ interruptions.length }}</span>
-                    <plus class="add-button" />
-                </div>
-            </input-panel>
-
-            <div class="interruption-node-items" :class="{ 'active-list': activeNode === 'interruption' }">
-                <task-summary-card v-for="interruption of interruptions"
-                    class="summary-card"
-                    :key="interruption.name"
-                    :task="interruption">
-                </task-summary-card>
-            </div>
-
-            <div class="active-indicator" v-if="activeNode === 'category'">
-                <div></div>
-            </div>
-
-            <input-panel class="category-node" @click="activeNode = 'category'">
-                <div class="node-content" :class="{ 'active-node': activeNode === 'category' }">
-                    <format-list-bulleted-type class="icon" />
-                    <span>Categories</span>
-                    <plus class="add-button" />
-                </div>
-            </input-panel>
-
-            <div class="category-node-items" :class="{ 'active-list': activeNode === 'category' }"></div>
+            <task-list class="task-list"
+                :isActive="activeButton === categoryButton"
+                :action="categoryButton"
+                @activate="activeButton = categoryButton">
+            </task-list>
         </div>
     </glass-panel>
 </template>
 
 <script lang="ts">
+import { markRaw } from 'vue';
 import { Options, Vue } from 'vue-class-component';
 import { ExclamationThick, FormatListBulletedType, Plus, TimerSand } from 'mdue';
 // eslint-disable-next-line no-unused-vars
 import { TaskItem } from '../../core/data-model/task-item';
+import { ActionButton } from '../../core/data-model/action-button';
 import { TaskItemHttpService } from '../../core/services/http/task-item-http/task-item-http.service';
-import TaskSummaryCard from '../../shared/cards/TaskSummaryCard.vue';
 import InputPanel from '../../shared/panels/InputPanel.vue';
 import GlassPanel from '../../shared/panels/GlassPanel.vue';
+import TaskList from '../../shared/components/TaskList.vue';
 
 const taskItemHttpService = new TaskItemHttpService();
 
 @Options({
     components: {
         ExclamationThick,
-        FormatListBulletedType,
         Plus,
         TimerSand,
-        TaskSummaryCard,
         InputPanel,
-        GlassPanel
+        GlassPanel,
+        TaskList
     }
 })
 export default class TaskSelector extends Vue {
-    public activeNode = 'task';
+    public taskButton = new ActionButton('Tasks', markRaw(TimerSand), 'rgb(255, 28, 82)');
+    public interruptionButton = new ActionButton('Interruptions', markRaw(ExclamationThick), 'rgb(0, 117, 255)');
+    public categoryButton = new ActionButton('Categories', markRaw(FormatListBulletedType), 'rgb(245, 238, 58)');
+    public activeButton = this.taskButton;
     public tasks: TaskItem[] = [];
     public interruptions: TaskItem[] = [];
 
@@ -99,25 +67,6 @@ export default class TaskSelector extends Vue {
 <style lang="scss" scoped>
 .task-selector-container {
 
-    .active-indicator {
-        position: relative;
-        margin-top: 0;
-        width: 100%;
-        height: 0;
-        animation: expandIndicatorVertical 0.2s ease forwards;
-
-        div {
-            position: absolute;
-            left: 42.5%;
-            width: 15%;
-            height: 100%;
-            background-color: rgb(246, 149, 78);
-            filter: brightness(1);
-            opacity: 0;
-            animation: expandIndicatorHorizontal 0.25s ease 0.1s forwards;
-        }
-    }
-
     .task-selector-content {
         display: flex;
         flex-direction: column;
@@ -126,145 +75,11 @@ export default class TaskSelector extends Vue {
         height: calc(100% - 5px);
     }
 
-    .task-node-items,
-    .interruption-node-items,
-    .category-node-items {
-        width: 100%;
-        flex: 0;
-        overflow-y: auto;
-        transition: flex 0.4s;
-    }
-
-    .summary-card {
-        width: 100%;
-        height: 8%;
-        max-height: 45px;
-        margin-bottom: 1px;
-        opacity: 0;
-    }
-
-    .active-list {
-        flex: 1;
-
-        .summary-card {
-            animation: revealSummary 0.3s ease 0.4s forwards;
-        }
-    }
-
-    .task-node,
-    .interruption-node,
-    .category-node {
+    ::v-deep .task-list {
         margin-top: 2px;
         width: 100%;
         height: 7%;
         max-height: 50px;
-
-        .node-content {
-            display: flex;
-            align-items: center;
-            padding: 0 0.75em;
-            width: calc(100% - 1.5em);
-            height: 100%;
-            background-color: rgba(63, 62, 68, 0.6);
-            color: rgba(255, 255, 255, 0.5);
-            font-family: 'Segoe UI';
-            font-size: 1.25em;
-            transition: color 0.3s, filter 0.3s, background-color 0.2s;
-
-            span {
-                margin-left: 0.5em;
-            }
-        }
-
-        .icon, .add-button {
-            font-size: 1.4em;
-            filter: brightness(0.7);
-        }
-
-        .add-button {
-            margin-left: auto;
-            color: rgb(246, 149, 78);
-            opacity: 0;
-            transition: filter 0.3s, opacity 0.3s;
-
-            &:hover {
-                filter: brightness(1);
-            }
-        }
-
-        .active-node {
-            background-color: rgb(63, 62, 68);
-            color: rgb(255, 255, 255);
-
-            .icon {
-                filter: brightness(1);
-            }
-        }
-
-        &:hover {
-            cursor: pointer;
-
-            .node-content {
-                background-color: rgb(63, 62, 68);
-                color: rgb(255, 255, 255);
-            }
-
-            .icon {
-                filter: brightness(1);
-            }
-
-            .add-button {
-                opacity: 1;
-            }
-        }
-    }
-
-    .task-node .icon {
-        color: rgb(255, 28, 82);
-    }
-
-    .interruption-node .icon {
-        color: rgb(0, 117, 255);
-    }
-
-    .category-node .icon {
-        color: rgb(245, 238, 58);
-    }
-
-    @keyframes expandIndicatorVertical {
-        from {
-            margin-top: 0;
-            height: 0;
-        }
-        to {
-            margin-top: 2px;
-            height: 0.25em;
-        }
-    }
-
-    @keyframes expandIndicatorHorizontal {
-        0% {
-            left: 42.5%;
-            width: 15%;
-            opacity: 0;
-        }
-        1% {
-            opacity: 1;
-        }
-        100% {
-            left: 0;
-            width: 100%;
-            opacity: 1;
-        }
-    }
-
-    @keyframes revealSummary {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
     }
 }
 </style>
