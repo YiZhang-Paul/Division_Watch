57013f895f0bd9158d1195486c77acaa54bdde09
YI ZHANG <scaccterran@gmail.com>
2021-01-20 20:00:30 -0500

    POM-58 category summary card component

---
+++ src/shared/cards/CategorySummaryCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="category-summary-card-container">
+        <span>{{ category.name }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, prop } from 'vue-class-component';
+// eslint-disable-next-line no-unused-vars
+import { Category } from '../../core/data-model/category';
+
+class CategorySummaryCardProp {
+    public category = prop<Category>({ default: null });
+}
+
+export default class CategorySummaryCard extends Vue.with(CategorySummaryCardProp) { }
+</script>
+
+<style lang="scss" scoped>
+.category-summary-card-container {
+    display: flex;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(85, 85, 85, 0.4);
+    font-size: 0.4rem;
+    transition: opacity 0.15s, color 0.3s;
+
+    &:hover {
+        cursor: pointer;
+        color: rgba(255, 255, 255);
+        filter: brightness(1.2);
+    }
+
+    & > span:first-of-type {
+        margin-left: 0.5rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+}
+</style>
