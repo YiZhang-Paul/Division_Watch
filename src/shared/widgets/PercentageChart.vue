1508f0665057e5e84c5d3ebe11f6eca37218b397
YI ZHANG <scaccterran@gmail.com>
2021-01-31 16:15:44 -0500

    POM-66 use only one bar handle

--- src/shared/widgets/PercentageChart.vue
+++ src/shared/widgets/PercentageChart.vue
@@ -1,17 +1,17 @@
 <template>
     <svg class="percentage-chart-container" width="100" height="100" viewBox="0 0 100 100" :style="chartStyle">
-        <circle class="bar" cx="50" cy="50" :r="useSimpleView ? 45.5 : 45" fill="none" :stroke="color" />
-
-        <circle :cx="useSimpleView ? 95.375 : 95"
+        <circle class="bar"
+            cx="50"
             cy="50"
-            :r="useSimpleView ? 1.25 : 2"
-            fill="rgb(255, 255, 255)" />
+            :r="useSimpleView ? 45.5 : 45"
+            fill="none"
+            :stroke="color" />
 
-        <circle class="bar-end"
+        <circle class="bar-handle"
             :cx="useSimpleView ? 95.375 : 95"
             cy="50"
             :r="useSimpleView ? 1.5 : 2"
-            :style="barEndStyle"
+            :style="barHandleStyle"
             fill="rgb(255, 255, 255)" />
     </svg>
 </template>
@@ -23,7 +23,8 @@ class PercentageChartProp {
     public useSimpleView = prop<boolean>({ default: false });
     public color = prop<string>({ default: 'rgb(255, 255, 255)' });
     public percentage = prop<number>({ default: 100 });
-    public delay = prop<number>({ default: 3500 });
+    public renderDelay = prop<number>({ default: 3200 });
+    public glowDelay = prop<number>({ default: 3000 });
 }
 
 @Options({
@@ -43,15 +44,18 @@ export default class PercentageChart extends Vue.with(PercentageChartProp) {
         };
     }
 
-    get barEndStyle(): { [key: string]: string | null } {
+    get barHandleStyle(): { [key: string]: string | null } {
         const degrees = 360 / 100 * this.percentage;
 
-        return { transform: this.canAnimate ? `rotate(${degrees}deg)` : null };
+        return {
+            'animation-delay': `${this.glowDelay / 1000}s`,
+            transform: this.canAnimate ? `rotate(${degrees}deg)` : null
+        };
     }
 
     public mounted(): void {
-        setTimeout(() => this.canAnimate = true, this.delay);
-        const element = document.querySelector('.bar-end');
+        setTimeout(() => this.canAnimate = true, this.renderDelay);
+        const element = document.querySelector('.bar-handle');
         element?.addEventListener('transitionend', () => this.$emit('chart:rendered'));
     }
 }
@@ -63,7 +67,7 @@ export default class PercentageChart extends Vue.with(PercentageChartProp) {
 
     transition: stroke-dashoffset $transition-time;
 
-    .bar, .bar-end {
+    .bar, .bar-handle {
         transform-origin: 50% 50%;
     }
 
@@ -77,8 +81,24 @@ export default class PercentageChart extends Vue.with(PercentageChartProp) {
         }
     }
 
-    .bar-end {
+    .bar-handle {
         transition: transform $transition-time;
+        animation: glow 5s ease-in-out forwards infinite;
+    }
+
+    @keyframes glow {
+        0% {
+            fill: rgb(255, 255, 255);
+        }
+        10% {
+            fill: rgb(141, 136, 156);
+        }
+        20% {
+            fill: rgb(255, 255, 255);
+        }
+        100% {
+            fill: rgb(255, 255, 255);
+        }
     }
 }
 </style>
