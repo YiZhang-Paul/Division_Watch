e4bc2e9524387ea42e771f4de16c2f100cc3b56f
YI ZHANG <scaccterran@gmail.com>
2021-02-04 23:34:09 -0500

    POM-75 view panel component

---
+++ src/shared/panels/ViewPanel.vue
@@ -0,0 +1,144 @@
+<template>
+    <div :class="{ [containerClass]: true, 'expanded-panel': stage >= 11 }">
+        <div class="content">
+            <slot></slot>
+        </div>
+
+        <div class="panel-box-wrapper">
+            <div class="panel-box" v-for="n in 4" :key="n"></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class ViewPanel extends Vue {
+    public readonly containerClass = 'view-panel-container';
+    public stage = 1;
+
+    public mounted(): void {
+        const container = document.querySelector(`.${this.containerClass}`);
+        container?.addEventListener('animationend', () => this.stage++);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.view-panel-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    border: 1px solid transparent;
+    background-color: transparent;
+    box-shadow: 3px 3px 3px 0 transparent;
+    transition: border-color 0.15s, background-color 0.15s, box-shadow 0.15s;
+    transition-delay: 0.15s;
+
+    &.expanded-panel {
+        border-color: rgba(170, 170, 170, 0.4);
+        background-color: rgba(160, 160, 160, 0.25);
+        box-shadow: 3px 3px 3px 0 rgba(120, 120, 120, 0.25);
+    }
+
+    .content {
+    }
+
+    .panel-box-wrapper {
+        $box-width: 0.75vw;
+        $box-height: 0.5vh;
+        $blink-duration: 0.25s;
+
+        position: absolute;
+        width: $box-width;
+        height: $box-height;
+        border-top: 1px solid rgba(200, 200, 200, 0.6);
+        border-bottom: 1px solid rgba(200, 200, 200, 0.6);
+        background-color: transparent;
+        animation: blinkFast $blink-duration ease-in forwards,
+                   expandPanelBox 1s ease 0.5s forwards;
+
+        .panel-box {
+            $box-move-duration: 0.2s;
+            $box-move-delay: calc(#{$blink-duration} + 0.2s);
+
+            position: absolute;
+            width: $box-width;
+            height: $box-height;
+            background-color: rgb(200, 200, 200);
+
+            &:nth-child(1) {
+                top: 0;
+                left: 0;
+                animation: movePanelBoxDown $box-move-duration ease $box-move-delay forwards,
+                           movePanelBoxLeft $box-move-duration ease $box-move-delay forwards;
+            }
+
+            &:nth-child(2) {
+                top: 0;
+                right: 0;
+                animation: movePanelBoxDown $box-move-duration ease $box-move-delay forwards,
+                           movePanelBoxRight $box-move-duration ease $box-move-delay forwards;
+            }
+
+            &:nth-child(3) {
+                bottom: 0;
+                left: 0;
+                animation: movePanelBoxUp $box-move-duration ease $box-move-delay forwards,
+                           movePanelBoxLeft $box-move-duration ease $box-move-delay forwards;
+            }
+
+            &:nth-child(4) {
+                bottom: 0;
+                right: 0;
+                animation: movePanelBoxUp $box-move-duration ease $box-move-delay forwards,
+                           movePanelBoxRight $box-move-duration ease $box-move-delay forwards;
+            }
+        }
+
+        @keyframes expandPanelBox {
+            0% {
+                width: $box-width;
+                height: $box-height;
+            }
+            20% {
+                width: 25%;
+                height: 1.5vh;
+            }
+            40% {
+                width: 75%;
+                height: 1.5vh;
+            }
+            65% {
+                width: 75%;
+                height: 1.5vh;
+            }
+            100% {
+                width: 95%;
+                height: 82.5%;
+            }
+        }
+
+        @keyframes movePanelBoxDown {
+            from { top: 0; }
+            to { top: 5px; }
+        }
+
+        @keyframes movePanelBoxUp {
+            from { bottom: 0; }
+            to { bottom: 5px; }
+        }
+
+        @keyframes movePanelBoxLeft {
+            from { left: 0; }
+            to { left: calc(#{$box-width} / 3 * -2); }
+        }
+
+        @keyframes movePanelBoxRight {
+            from { right: 0; }
+            to { right: calc(#{$box-width} / 3 * -2); }
+        }
+    }
+}
+</style>
