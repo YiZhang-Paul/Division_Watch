3d4c2e759f31798d5a450fd60354ad86d3ec722c
YI ZHANG <scaccterran@gmail.com>
2021-02-06 11:58:40 -0500

    POM-75 tab group

---
+++ src/shared/controls/TabGroup.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="tab-group-container">
+        <tab-button class="tab-button"
+            v-for="(option, index) of options"
+            :key="index"
+            :isActive="activeIndex === index"
+            :badgeValue="option.badgeValue"
+            @click="onSelect(index)">
+
+            {{ option.name }}
+        </tab-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component';
+// eslint-disable-next-line no-unused-vars
+import { TabGroupOption } from '../../core/data-model/generic/tab-group-option';
+
+import TabButton from './TabButton.vue';
+
+class TabGroupProp {
+    public options = prop<TabGroupOption[]>({ default: [] });
+}
+
+@Options({
+    components: {
+        TabButton
+    },
+    emits: ['tab:selected']
+})
+export default class TabGroup extends Vue.with(TabGroupProp) {
+    public activeIndex = 0;
+
+    public onSelect(index: number): void {
+        this.activeIndex = index;
+        this.$emit('tab:selected', this.options[index].name);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tab-group-container {
+    display: flex;
+
+    .tab-button:not(:nth-last-child(1)) {
+        margin-right: 1vh;
+    }
+}
+</style>
