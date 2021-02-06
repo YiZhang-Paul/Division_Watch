1d2b5b6d5fa5a0c795b3e949b27d73cc1105171c
YI ZHANG <scaccterran@gmail.com>
2021-02-06 15:10:32 -0500

    POM-75 task manager and item list panel

---
+++ src/features/activity-manager/TaskManager.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="task-manager-container">
+        <item-list-panel class="item-list-panel">
+            <task-summary-card class="summary-card"
+                v-for="task of tasks"
+                :key="task.id"
+                :task="task">
+            </task-summary-card>
+        </item-list-panel>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+import store from '../../store';
+import { taskItemKey } from '../../store/task-item/task-item.state';
+// eslint-disable-next-line no-unused-vars
+import { TaskItem } from '../../core/data-model/task-item/task-item';
+import ItemListPanel from '../../shared/panels/ItemListPanel.vue';
+import TaskSummaryCard from '../../shared/cards/TaskSummaryCard.vue';
+
+@Options({
+    components: {
+        ItemListPanel,
+        TaskSummaryCard
+    }
+})
+export default class TaskManager extends Vue {
+
+    get tasks(): TaskItem[] {
+        return store.getters[`${taskItemKey}/incompleteParentTasks`];
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.task-manager-container {
+
+    .item-list-panel {
+        width: 37.5%;
+        height: 95%;
+
+        .summary-card {
+            margin-bottom: 1vh;
+            width: 100%;
+            height: 12.5vh;
+        }
+    }
+}
+</style>
---
+++ src/shared/panels/ItemListPanel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="item-list-panel-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class ItemListPanel extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+.item-list-panel-container {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid red;
+}
+</style>
