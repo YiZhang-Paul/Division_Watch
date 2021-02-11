00f7f63cdab4a95ddf7dcd0d6bfe8ce182b0eba9
YI ZHANG <scaccterran@gmail.com>
2021-02-10 21:21:04 -0500

    POM-14 extracted editor fields into separate component

---
+++ src/features/activity-manager/TaskEditor.vue
@@ -0,0 +1,236 @@
+<template>
+    <div v-if="task" class="task-editor-container">
+        <section-panel class="basic-information"
+            :name="task.name"
+            :isEditable="true"
+            :placeholder="'enter task name here...'"
+            @name:edited="onTaskChange('name', $event)">
+
+            <option-dropdown class="editor-control"
+                :name="'Category'"
+                :selected="selectedCategory"
+                :options="categories"
+                :transform="_ => _.name"
+                @options:select="onTaskChange('categoryId', $event.id)">
+            </option-dropdown>
+
+            <option-dropdown class="editor-control"
+                :name="'Priority'"
+                :selected="task.priority"
+                :options="taskOptions.priorities"
+                :transform="_ => _.name"
+                @options:select="onTaskChange('priority', $event)">
+            </option-dropdown>
+
+            <option-dropdown class="editor-control"
+                :name="'Deadline'"
+                :selected="task.deadline"
+                :options="taskOptions.deadlines"
+                :transform="toDisplayDate"
+                @options:select="onTaskChange('deadline', $event)">
+            </option-dropdown>
+
+            <option-dropdown class="editor-control"
+                :name="'Estimate'"
+                :selected="task.estimate"
+                :options="taskOptions.estimates"
+                :isDisabled="childTasks.length"
+                :disableText="childTasksEstimation"
+                :transform="toDisplayEstimation"
+                @options:select="onTaskChange('estimate', $event)">
+            </option-dropdown>
+
+            <day-selector class="editor-control"
+                :name="'Recur'"
+                :days="task.recur.slice()"
+                :isDisabled="task.parent"
+                @days:select="onTaskChange('recur', $event)">
+            </day-selector>
+        </section-panel>
+
+        <div class="subsections">
+            <item-group-panel class="child-tasks"
+                :name="'Subtasks (' + childTasks.length + ')'"
+                :placeholder="'add child task here...'"
+                @item:add="onChildTaskAdded($event)">
+
+                <subtask-summary-card class="subtask-summary-card"
+                    v-for="task of childTasks"
+                    :key="task.id"
+                    :task="task">
+                </subtask-summary-card>
+            </item-group-panel>
+
+            <item-group-panel class="checklist-items"
+                :name="checklistTitle"
+                :placeholder="'add checklist here...'"
+                @item:add="onChecklistAdded($event)">
+
+                <checklist-card class="checklist-card"
+                    v-for="(item, index) of task.checklist"
+                    :key="item.description"
+                    :item="item"
+                    @change="onChecklistChange(index, $event)"
+                    @delete="onChecklistDelete(index)">
+                </checklist-card>
+            </item-group-panel>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component';
+
+import store from '../../store';
+import { dialogKey } from '../../store/dialog/dialog.state';
+import { categoryKey } from '../../store/category/category.state';
+import { taskItemKey } from '../../store/task-item/task-item.state';
+import { Category } from '../../core/data-model/generic/category';
+// eslint-disable-next-line no-unused-vars
+import { TaskItem } from '../../core/data-model/task-item/task-item';
+// eslint-disable-next-line no-unused-vars
+import { TaskItemOptions } from '../../core/data-model/task-item/task-item-options';
+import { ChecklistItem } from '../../core/data-model/task-item/checklist-item';
+import { DialogOption } from '../../core/data-model/generic/dialog-option';
+import ItemGroupPanel from '../../shared/panels/ItemGroupPanel.vue';
+import SectionPanel from '../../shared/panels/SectionPanel.vue';
+import OptionDropdown from '../../shared/controls/OptionDropdown.vue';
+import DaySelector from '../../shared/controls/DaySelector.vue';
+import SubtaskSummaryCard from '../../shared/cards/SubtaskSummaryCard.vue';
+import ChecklistCard from '../../shared/cards/ChecklistCard.vue';
+import { TimeUtility } from '../../core/utilities/time/time.utility';
+import { GenericUtility } from '../../core/utilities/generic/generic.utility';
+
+class TaskEditorProp {
+    public task = prop<TaskItem>({ default: null });
+    public childTasks = prop<TaskItem[]>({ default: [] });
+}
+
+@Options({
+    components: {
+        ItemGroupPanel,
+        SectionPanel,
+        OptionDropdown,
+        DaySelector,
+        SubtaskSummaryCard,
+        ChecklistCard
+    },
+    emits: [
+        'task:change',
+        'child:add'
+    ]
+})
+export default class TaskEditor extends Vue.with(TaskEditorProp) {
+
+    get selectedCategory(): Category {
+        return store.getters[`${categoryKey}/category`](this.task?.categoryId);
+    }
+
+    get categories(): Category[] {
+        const categories: Category[] = store.getters[`${categoryKey}/categories`];
+
+        return [new Category('N/A'), ...categories];
+    }
+
+    get taskOptions(): TaskItemOptions {
+        return store.getters[`${taskItemKey}/taskItemOptions`];
+    }
+
+    get childTasksEstimation(): string {
+        const estimation = this.childTasks.reduce((total, _) => total + _.estimate, 0);
+
+        return this.toDisplayEstimation(estimation);
+    }
+
+    get checklistTitle(): string {
+        if (!this.task?.checklist) {
+            return '';
+        }
+
+        const completed = this.task.checklist.filter(_ => _.isCompleted).length;
+
+        return `Checklist (${completed}/${this.task.checklist.length})`;
+    }
+
+    public onChildTaskAdded(name: string): void {
+        this.$emit('child:add', { ...new TaskItem(), name });
+    }
+
+    public onChecklistAdded(name: string): void {
+        const checklist = this.task?.checklist ?? [];
+        this.onTaskChange('checklist', [...checklist, new ChecklistItem(name)]);
+    }
+
+    public onChecklistChange(index: number, item: ChecklistItem): void {
+        const checklist = this.task?.checklist ?? [];
+        this.onTaskChange('checklist', GenericUtility.replaceAt(checklist, item, index));
+    }
+
+    public onChecklistDelete(index: number): void {
+        const title = 'This item will be permanently deleted.';
+        const option = new DialogOption(title, 'Delete', 'Cancel', true);
+
+        option.confirmCallback = () => {
+            const checklist = this.task?.checklist ?? [];
+            this.onTaskChange('checklist', GenericUtility.removeAt(checklist, index), 200);
+        };
+
+        store.dispatch(`${dialogKey}/openDialog`, option);
+    }
+
+    public onTaskChange(key: string, value: any, delay = 0): void {
+        setTimeout(() => this.$emit('task:change', { ...this.task, [key]: value }), delay);
+    }
+
+    public toDisplayDate(raw: string): string {
+        return raw ? TimeUtility.toShortDateString(raw) : 'N/A';
+    }
+
+    public toDisplayEstimation(time: number): string {
+        return TimeUtility.toEstimationString(time, this.taskOptions.skullDuration);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.task-editor-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    .basic-information {
+        width: 100%;
+        height: 42%;
+
+        .editor-control {
+            width: 100%;
+        }
+    }
+
+    .subsections {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 1.75%;
+        width: 100%;
+        height: 39.5%;
+
+        .child-tasks, .checklist-items {
+            width: 48.75%;
+            height: 100%;
+        }
+
+        .subtask-summary-card, .checklist-card {
+            width: 100%;
+            height: 4.5vh;
+            opacity: 0;
+            animation: revealContent 0.3s ease 0.1s forwards;
+
+            &:not(:nth-last-child(1)) {
+                margin-bottom: 1%;
+            }
+        }
+    }
+}
+</style>
--- src/features/activity-manager/TaskManager.vue
+++ src/features/activity-manager/TaskManager.vue
@@ -13,82 +13,13 @@
             </task-summary-card>
         </item-list-panel>
 
-        <div v-if="activeTask" class="editor-area">
-            <section-panel class="basic-information"
-                :name="activeTask.name"
-                :isEditable="true"
-                :placeholder="'enter task name here...'"
-                @name:edited="onItemChange('name', $event)">
-
-                <option-dropdown class="editor-control"
-                    :name="'Category'"
-                    :selected="selectedCategory"
-                    :options="categories"
-                    :transform="_ => _.name"
-                    @options:select="onItemChange('categoryId', $event.id)">
-                </option-dropdown>
-
-                <option-dropdown class="editor-control"
-                    :name="'Priority'"
-                    :selected="activeTask.priority"
-                    :options="taskOptions.priorities"
-                    :transform="_ => _.name"
-                    @options:select="onItemChange('priority', $event)">
-                </option-dropdown>
-
-                <option-dropdown class="editor-control"
-                    :name="'Deadline'"
-                    :selected="activeTask.deadline"
-                    :options="taskOptions.deadlines"
-                    :transform="toDisplayDate"
-                    @options:select="onItemChange('deadline', $event)">
-                </option-dropdown>
-
-                <option-dropdown class="editor-control"
-                    :name="'Estimate'"
-                    :selected="activeTask.estimate"
-                    :options="taskOptions.estimates"
-                    :isDisabled="activeChildTasks.length"
-                    :disableText="activeChildTasksEstimation"
-                    :transform="toDisplayEstimation"
-                    @options:select="onItemChange('estimate', $event)">
-                </option-dropdown>
-
-                <day-selector class="editor-control"
-                    :name="'Recur'"
-                    :days="activeTask.recur.slice()"
-                    :isDisabled="activeTask.parent"
-                    @days:select="onItemChange('recur', $event)">
-                </day-selector>
-            </section-panel>
-
-            <div class="subsections">
-                <item-group-panel class="child-tasks"
-                    :name="'Subtasks (' + activeChildTasks.length + ')'"
-                    :placeholder="'add child task here...'"
-                    @item:add="addChildTask($event)">
-
-                    <subtask-summary-card class="subtask-summary-card"
-                        v-for="task of activeChildTasks"
-                        :key="task.id"
-                        :task="task">
-                    </subtask-summary-card>
-                </item-group-panel>
-
-                <item-group-panel class="checklist-items"
-                    :name="checklistTitle"
-                    :placeholder="'add checklist here...'"
-                    @item:add="addChecklistItem($event)">
-
-                    <checklist-card class="checklist-card"
-                        v-for="(item, index) of activeTask.checklist"
-                        :key="item.description"
-                        :item="item"
-                        @change="onChecklistChange(index, $event)"
-                        @delete="onChecklistDelete(index)">
-                    </checklist-card>
-                </item-group-panel>
-            </div>
+        <div v-if="activeTask" class="content">
+            <task-editor class="task-editor"
+                :task="activeTask"
+                :childTasks="activeChildTasks"
+                @task:change="onTaskChange($event)"
+                @child:add="addChildTask($event)">
+            </task-editor>
 
             <actions-group class="actions-group"
                 :name="'Danger Zone'"
@@ -105,40 +36,24 @@ import { Options, Vue } from 'vue-class-component';
 
 import store from '../../store';
 import { dialogKey } from '../../store/dialog/dialog.state';
-import { categoryKey } from '../../store/category/category.state';
 import { taskItemKey } from '../../store/task-item/task-item.state';
-import { Category } from '../../core/data-model/generic/category';
-import { ChecklistItem } from '../../core/data-model/task-item/checklist-item';
 // eslint-disable-next-line no-unused-vars
 import { TaskItem } from '../../core/data-model/task-item/task-item';
-// eslint-disable-next-line no-unused-vars
-import { TaskItemOptions } from '../../core/data-model/task-item/task-item-options';
 import { BasicAction } from '../../core/data-model/generic/basic-action';
 import { DialogOption } from '../../core/data-model/generic/dialog-option';
 import ItemListPanel from '../../shared/panels/ItemListPanel.vue';
-import SectionPanel from '../../shared/panels/SectionPanel.vue';
-import ItemGroupPanel from '../../shared/panels/ItemGroupPanel.vue';
 import ActionsGroup from '../../shared/controls/ActionsGroup.vue';
-import OptionDropdown from '../../shared/controls/OptionDropdown.vue';
-import DaySelector from '../../shared/controls/DaySelector.vue';
 import TaskSummaryCard from '../../shared/cards/TaskSummaryCard.vue';
-import SubtaskSummaryCard from '../../shared/cards/SubtaskSummaryCard.vue';
-import ChecklistCard from '../../shared/cards/ChecklistCard.vue';
 import { TaskAction } from '../../core/enums/task-action.enum';
-import { TimeUtility } from '../../core/utilities/time/time.utility';
-import { GenericUtility } from '../../core/utilities/generic/generic.utility';
+
+import TaskEditor from './TaskEditor.vue';
 
 @Options({
     components: {
         ItemListPanel,
-        SectionPanel,
-        ItemGroupPanel,
         ActionsGroup,
-        OptionDropdown,
-        DaySelector,
         TaskSummaryCard,
-        SubtaskSummaryCard,
-        ChecklistCard
+        TaskEditor
     }
 })
 export default class TaskManager extends Vue {
@@ -146,18 +61,10 @@ export default class TaskManager extends Vue {
     public searchText = '';
     private updateDebounceTimer: NodeJS.Timeout | null = null;
 
-    get taskOptions(): TaskItemOptions {
-        return store.getters[`${taskItemKey}/taskItemOptions`];
-    }
+    get tasks(): TaskItem[] {
+        const tasks: TaskItem[] = store.getters[`${taskItemKey}/incompleteParentTasks`];
 
-    get categories(): Category[] {
-        const categories: Category[] = store.getters[`${categoryKey}/categories`];
-
-        return [new Category('N/A'), ...categories];
-    }
-
-    get selectedCategory(): Category {
-        return store.getters[`${categoryKey}/category`](this.activeTask?.categoryId);
+        return tasks.filter(_ => _.name.toLowerCase().includes(this.searchText));
     }
 
     get activeTask(): TaskItem | null {
@@ -170,29 +77,6 @@ export default class TaskManager extends Vue {
         }
 
         return store.getters[`${taskItemKey}/incompleteChildTasksByParentId`](this.activeTask.id);
-    }
-
-    get activeChildTasksEstimation(): string {
-        const estimation = this.activeChildTasks.reduce((total, _) => total + _.estimate, 0);
-
-        return this.toDisplayEstimation(estimation);
-    }
-
-    get checklistTitle(): string {
-        if (!this.activeTask?.checklist) {
-            return '';
-        }
-
-        const { checklist } = this.activeTask;
-        const completed = checklist.filter(_ => _.isCompleted).length;
-
-        return `Checklist (${completed}/${checklist.length})`;
-    }
-
-    get tasks(): TaskItem[] {
-        const tasks: TaskItem[] = store.getters[`${taskItemKey}/incompleteParentTasks`];
-
-        return tasks.filter(_ => _.name.toLowerCase().includes(this.searchText));
     }
 
     public beforeUnmount(): void {
@@ -213,11 +97,10 @@ export default class TaskManager extends Vue {
         store.dispatch(`${taskItemKey}/swapActiveItem`, task);
     }
 
-    public onItemChange(key: string, value: any, delay = 0): void {
-        const changed = { ...this.activeTask!, [key]: value };
-        setTimeout(() => store.commit(`${taskItemKey}/setActiveItem`, changed), delay);
+    public onTaskChange(task: TaskItem): void {
+        store.commit(`${taskItemKey}/setActiveItem`, task);
 
-        if (!changed.id) {
+        if (!task.id) {
             return;
         }
 
@@ -231,21 +114,11 @@ export default class TaskManager extends Vue {
         }, 1000);
     }
 
-    public onChecklistChange(index: number, item: ChecklistItem): void {
-        const checklist = this.activeTask?.checklist ?? [];
-        this.onItemChange('checklist', GenericUtility.replaceAt(checklist, item, index));
-    }
-
-    public onChecklistDelete(index: number): void {
-        const title = 'This item will be permanently deleted.';
-        const option = new DialogOption(title, 'Delete', 'Cancel', true);
-
-        option.confirmCallback = () => {
-            const checklist = this.activeTask?.checklist ?? [];
-            this.onItemChange('checklist', GenericUtility.removeAt(checklist, index), 200);
-        };
-
-        store.dispatch(`${dialogKey}/openDialog`, option);
+    public async addChildTask(task: TaskItem): Promise<void> {
+        if (this.activeTask) {
+            const payload = { parentId: this.activeTask.id, item: task };
+            await store.dispatch(`${taskItemKey}/addChildTaskItem`, payload);
+        }
     }
 
     public onActionSelected(action: TaskAction): void {
@@ -259,29 +132,6 @@ export default class TaskManager extends Vue {
 
             store.dispatch(`${dialogKey}/openDialog`, option);
         }
-    }
-
-    public async addChildTask(name: string): Promise<void> {
-        if (this.activeTask) {
-            const child: TaskItem = { ...new TaskItem(), name };
-            const payload = { parentId: this.activeTask.id, task: child };
-            await store.dispatch(`${taskItemKey}/addChildTaskItem`, payload);
-        }
-    }
-
-    public addChecklistItem(name: string): void {
-        if (this.activeTask) {
-            this.activeTask.checklist.push(new ChecklistItem(name));
-            store.dispatch(`${taskItemKey}/updateTaskItem`, this.activeTask);
-        }
-    }
-
-    public toDisplayDate(raw: string): string {
-        return raw ? TimeUtility.toShortDateString(raw) : 'N/A';
-    }
-
-    public toDisplayEstimation(time: number): string {
-        return TimeUtility.toEstimationString(time, this.taskOptions.skullDuration);
     }
 }
 </script>
@@ -306,7 +156,7 @@ export default class TaskManager extends Vue {
         }
     }
 
-    .editor-area {
+    .content {
         $content-width: 92.5%;
 
         display: flex;
@@ -315,37 +165,8 @@ export default class TaskManager extends Vue {
         width: calc(100% - #{$list-width});
         height: 100%;
 
-        .basic-information {
+        .task-editor {
             width: $content-width;
-            height: 42%;
-
-            .editor-control {
-                width: 100%;
-            }
-        }
-
-        .subsections {
-            display: flex;
-            justify-content: space-between;
-            margin-top: 1.75%;
-            width: $content-width;
-            height: 39.5%;
-
-            .child-tasks, .checklist-items {
-                width: 48.75%;
-                height: 100%;
-            }
-
-            .subtask-summary-card, .checklist-card {
-                width: 100%;
-                height: 4.5vh;
-                opacity: 0;
-                animation: revealContent 0.3s ease 0.1s forwards;
-
-                &:not(:nth-last-child(1)) {
-                    margin-bottom: 1%;
-                }
-            }
         }
 
         .actions-group {
