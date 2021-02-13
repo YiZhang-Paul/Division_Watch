dbaba4f5e68348926e95d713df1c1205e7737d3b
YI ZHANG <scaccterran@gmail.com>
2021-02-13 15:44:07 -0500

    POM-85 category summary card component

---
+++ src/shared/cards/CategorySummaryCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="category-summary-card-container"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component';
+// eslint-disable-next-line no-unused-vars
+import { Category } from '../../core/data-model/generic/category';
+
+class CategorySummaryCardProp {
+    public category = prop<Category>({ default: null });
+    public isActive = prop<boolean>({ default: false });
+}
+
+@Options({
+
+})
+export default class CategorySummaryCard extends Vue.with(CategorySummaryCardProp) { }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
