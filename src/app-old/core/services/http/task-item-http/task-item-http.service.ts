33ca602410cc28e0ffb7bfaf3ca790f687be597b
YI ZHANG <scaccterran@gmail.com>
2021-02-14 16:14:07 -0500

    POM-85 removed old code

--- src/app-old/core/services/http/task-item-http/task-item-http.service.ts
+++
@@ -1,7 +0,0 @@
-export class TaskItemHttpService {
-    public async getTaskItem(id: string): Promise<TaskItem> {
-        const endpoint = `${this._api}/${id}`;
-
-        return (await axios.get(endpoint)).data;
-    }
-}
