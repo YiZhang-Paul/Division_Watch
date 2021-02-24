de2bce9aa1f77d72b478b4eaf64410f37e1f8f3e
YI ZHANG <scaccterran@gmail.com>
2021-02-23 21:23:46 -0500

    POM-37 app settings state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import { createStore } from 'vuex';
 
 import { IDialogState, dialog, dialogKey } from './dialog/dialog.state';
+import { ISettingsState, settings, settingsKey } from './settings/settings.state';
 import { IMainViewState, mainView, mainViewKey } from './main-view/main-view.state';
 import { ICategoryState, category, categoryKey } from './category/category.state';
 import { ITaskItemState, taskItem, taskItemKey } from './task-item/task-item.state';
 import { IWatchBaseState, watchBase, watchBaseKey } from './watch-base/watch-base.state';
 
 type StoreType = {
-    [dialogKey]: IDialogState,
+    [dialogKey]: IDialogState;
+    [settingsKey]: ISettingsState;
     [mainViewKey]: IMainViewState;
     [categoryKey]: ICategoryState;
     [taskItemKey]: ITaskItemState;
@@ -17,6 +19,7 @@ type StoreType = {
 export default createStore<StoreType>({
     modules: {
         [dialogKey]: dialog,
+        [settingsKey]: settings,
         [mainViewKey]: mainView,
         [categoryKey]: category,
         [taskItemKey]: taskItem,
---
+++ src/store/settings/settings.state.ts
@@ -0,0 +1,50 @@
+import { ActionContext } from 'vuex';
+
+import { SessionSettings } from '../../core/data-model/settings/session-settings';
+import { SessionSettingsOptions } from '../../core/data-model/settings/session-settings-options';
+import { AppSettingsHttpService } from '../../core/services/http/app-settings-http/app-settings-http.service';
+
+const appSettingsHttpService = new AppSettingsHttpService();
+
+export interface ISettingsState {
+    sessionSettings: SessionSettings;
+    sessionSettingsOptions: SessionSettingsOptions;
+}
+
+const state = (): ISettingsState => ({
+    sessionSettings: new SessionSettings(),
+    sessionSettingsOptions: new SessionSettingsOptions()
+});
+
+const getters = {
+    sessionSettings: (state: ISettingsState): SessionSettings => state.sessionSettings,
+    sessionSettingsOptions: (state: ISettingsState): SessionSettingsOptions => state.sessionSettingsOptions
+};
+
+const mutations = {
+    setSessionSettings(state: ISettingsState, settings: SessionSettings): void {
+        state.sessionSettings = settings;
+    },
+    setSessionSettingsOptions(state: ISettingsState, options: SessionSettingsOptions): void {
+        state.sessionSettingsOptions = options;
+    }
+};
+
+const actions = {
+    async loadSessionSettings(context: ActionContext<ISettingsState, any>): Promise<void> {
+        context.commit('setSessionSettings', await appSettingsHttpService.getSessionSettings());
+    },
+    async loadSessionSettingsOptions(context: ActionContext<ISettingsState, any>): Promise<void> {
+        context.commit('setSessionSettingsOptions', await appSettingsHttpService.getSessionSettingsOptions());
+    }
+};
+
+export const settingsKey = 'settings';
+
+export const settings = {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+};
