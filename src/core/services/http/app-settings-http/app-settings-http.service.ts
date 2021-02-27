192ebdefc4551606b849a84ab5b4fa2345240d37
YI ZHANG <scaccterran@gmail.com>
2021-02-27 14:44:06 -0500

    POM-38 api service for sound settings

--- src/core/services/http/app-settings-http/app-settings-http.service.ts
+++ src/core/services/http/app-settings-http/app-settings-http.service.ts
@@ -1,10 +1,39 @@
 import axios from 'axios';
 
+import { SoundSettings } from '../../../data-model/settings/sound-settings';
+import { SoundSettingsOptions } from '../../../data-model/settings/sound-settings-options';
 import { SessionSettings } from '../../../data-model/settings/session-settings';
 import { SessionSettingsOptions } from '../../../data-model/settings/session-settings-options';
 
 export class AppSettingsHttpService {
     private readonly _api = `${process.env.VUE_APP_BASE_API_URL}/app-settings`;
+
+    public async getSoundSettings(): Promise<SoundSettings> {
+        try {
+            return (await axios.get(`${this._api}/sound`)).data;
+        }
+        catch {
+            return new SoundSettings();
+        }
+    }
+
+    public async getSoundSettingsOptions(): Promise<SoundSettingsOptions> {
+        try {
+            return (await axios.get(`${this._api}/sound/options`)).data;
+        }
+        catch {
+            return new SoundSettingsOptions();
+        }
+    }
+
+    public async updateSoundSettings(settings: SoundSettings): Promise<boolean> {
+        try {
+            return (await axios.put(`${this._api}/sound`, settings)).data;
+        }
+        catch {
+            return false;
+        }
+    }
 
     public async getSessionSettings(): Promise<SessionSettings> {
         try {
