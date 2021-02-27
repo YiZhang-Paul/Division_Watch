17ba325c78ed380fba221b9a64c3436d06470a10
YI ZHANG <scaccterran@gmail.com>
2021-02-26 22:21:38 -0500

    POM-80 simplified sound class and changed default file type to .wav

--- src/core/data-model/generic/sound.ts
+++ src/core/data-model/generic/sound.ts
@@ -1,44 +1,26 @@
 export class Sound {
     public readonly name: string;
     private file: HTMLAudioElement;
-    private timeoutTimer: NodeJS.Timeout | null = null;
-    private intervalTimer: NodeJS.Timeout | null = null;
     private _volume = 1;
 
     constructor(name: string) {
         this.name = name;
-        this.file = new Audio(`division-protocol://src/assets/sounds/${name}.mp3`);
+        this.file = new Audio(`division-protocol://src/assets/sounds/${name}.wav`);
     }
 
     set volume(value: number) {
         this._volume = Math.max(0, Math.min(value, 1));
     }
 
-    public loop(interval = 1000): void {
-        this.intervalTimer = setTimeout(() => this.loop(interval), interval);
-        this.play(0);
-    }
-
-    public play(delay = 0): void {
-        if (this.timeoutTimer) {
-            clearTimeout(this.timeoutTimer);
-        }
-
-        this.timeoutTimer = setTimeout(() => {
-            this.file.volume = this._volume;
-            this.file.play();
-        }, delay);
+    public play(loop: boolean): void {
+        this.file.volume = this._volume;
+        this.file.loop = loop;
+        this.file.play();
     }
 
     public stop(): void {
-        if (this.timeoutTimer) {
-            clearTimeout(this.timeoutTimer);
-            this.timeoutTimer = null;
-        }
-
-        if (this.intervalTimer) {
-            clearTimeout(this.intervalTimer);
-            this.intervalTimer = null;
-        }
+        this.file.pause();
+        this.file.currentTime = 0;
+        this.file.loop = false;
     }
 }
