dcab31a7b7402f891a9a8468a09916132117c867
YI ZHANG <scaccterran@gmail.com>
2021-02-26 22:22:41 -0500

    POM-80 simplified sound state and separated different type of sounds

--- src/store/sound/sound.state.ts
+++ src/store/sound/sound.state.ts
@@ -1,64 +1,59 @@
 import { ActionContext } from 'vuex';
 
 import { Sound } from '../../core/data-model/generic/sound';
+import { SoundOption } from '../../core/data-model/generic/sound-option';
+import { SoundType } from '../../core/enums/sound-type.enum';
 
 export interface ISoundState {
-    masterVolume: number;
     isMuted: boolean;
-    sounds: Sound[];
+    masterVolume: number;
+    clockSounds: { sounds: Sound[]; volume: number };
 }
 
 const state = (): ISoundState => ({
-    masterVolume: 1,
     isMuted: false,
-    sounds: []
+    masterVolume: 0.5,
+    clockSounds: { sounds: [], volume: 1 }
 });
 
 const getters = {
-    getSound: (state: ISoundState) => (name: string): Sound | null => {
-        return state.sounds.find(_ => _.name === name) ?? null;
+    getClockSound: (state: ISoundState) => (name: string): Sound | null => {
+        return state.clockSounds.sounds.find(_ => _.name === name) ?? null;
     }
 };
 
 const mutations = {
-    setMasterVolume(state: ISoundState, volume: number): void {
-        state.masterVolume = Math.max(0, Math.min(volume, 1));
-    },
     setIsMuted(state: ISoundState, isMuted: boolean): void {
         state.isMuted = isMuted;
     },
-    addSound(state: ISoundState, sound: Sound): void {
-        state.sounds = [...state.sounds, sound];
+    setMasterVolume(state: ISoundState, volume: number): void {
+        state.masterVolume = Math.max(0, Math.min(volume, 1));
+    },
+    addClockSound(state: ISoundState, sound: Sound): void {
+        sound.volume = state.masterVolume * state.clockSounds.volume;
+        state.clockSounds.sounds = [...state.clockSounds.sounds, sound];
     }
 };
 
 const actions = {
-    playSound(context: ActionContext<ISoundState, any>, name: string): void {
-        const { state, getters, commit } = context;
-        let sound: Sound = getters.getSound(name);
+    playSound(context: ActionContext<ISoundState, any>, option: SoundOption): void {
+        const { getters, commit } = context;
+        const { name, type, loop } = option;
+        let sound: Sound | null = null;
+
+        if (type === SoundType.Clock) {
+            sound = getters.getClockSound(name);
+        }
 
         if (!sound) {
             sound = new Sound(name);
-            sound.volume = state.masterVolume;
-            commit('addSound', sound);
+
+            if (type === SoundType.Clock) {
+                commit('addClockSound', sound);
+            }
         }
 
-        sound.play(0);
-    },
-    loopSound(context: ActionContext<ISoundState, any>, payload: { name: string; interval: number }): void {
-        const { state, getters, commit } = context;
-        let sound: Sound = getters.getSound(payload.name);
-
-        if (!sound) {
-            sound = new Sound(payload.name);
-            sound.volume = state.masterVolume;
-            commit('addSound', sound);
-        }
-
-        sound.loop(payload.interval);
-    },
-    stopSound(context: ActionContext<ISoundState, any>, name: string): void {
-        context.getters.getSound(name)?.stop();
+        sound.play(loop);
     }
 };
 
