164abd2db08bfbb1ec2a552dfadb71b95c6807cc
YI ZHANG <scaccterran@gmail.com>
2021-02-27 14:44:37 -0500

    POM-38 added sound settings in data store

--- src/store/settings/settings.state.ts
+++ src/store/settings/settings.state.ts
@@ -1,5 +1,7 @@
 import { ActionContext } from 'vuex';
 
+import { SoundSettings } from '../../core/data-model/settings/sound-settings';
+import { SoundSettingsOptions } from '../../core/data-model/settings/sound-settings-options';
 import { SessionSettings } from '../../core/data-model/settings/session-settings';
 import { SessionSettingsOptions } from '../../core/data-model/settings/session-settings-options';
 import { AppSettingsHttpService } from '../../core/services/http/app-settings-http/app-settings-http.service';
@@ -7,21 +9,33 @@ import { AppSettingsHttpService } from '../../core/services/http/app-settings-ht
 const appSettingsHttpService = new AppSettingsHttpService();
 
 export interface ISettingsState {
+    soundSettings: SoundSettings;
+    soundSettingsOptions: SoundSettingsOptions;
     sessionSettings: SessionSettings;
     sessionSettingsOptions: SessionSettingsOptions;
 }
 
 const state = (): ISettingsState => ({
+    soundSettings: new SoundSettings(),
+    soundSettingsOptions: new SoundSettingsOptions(),
     sessionSettings: new SessionSettings(),
     sessionSettingsOptions: new SessionSettingsOptions()
 });
 
 const getters = {
+    soundSettings: (state: ISettingsState): SoundSettings => state.soundSettings,
+    soundSettingsOptions: (state: ISettingsState): SoundSettingsOptions => state.soundSettingsOptions,
     sessionSettings: (state: ISettingsState): SessionSettings => state.sessionSettings,
     sessionSettingsOptions: (state: ISettingsState): SessionSettingsOptions => state.sessionSettingsOptions
 };
 
 const mutations = {
+    setSoundSettings(state: ISettingsState, settings: SoundSettings): void {
+        state.soundSettings = settings;
+    },
+    setSoundSettingsOptions(state: ISettingsState, options: SoundSettingsOptions): void {
+        state.soundSettingsOptions = options;
+    },
     setSessionSettings(state: ISettingsState, settings: SessionSettings): void {
         state.sessionSettings = settings;
     },
@@ -31,6 +45,15 @@ const mutations = {
 };
 
 const actions = {
+    async loadSoundSettings(context: ActionContext<ISettingsState, any>): Promise<void> {
+        context.commit('setSoundSettings', await appSettingsHttpService.getSoundSettings());
+    },
+    async loadSoundSettingsOptions(context: ActionContext<ISettingsState, any>): Promise<void> {
+        context.commit('setSoundSettingsOptions', await appSettingsHttpService.getSoundSettingsOptions());
+    },
+    async updateSoundSettings(_: ActionContext<ISettingsState, any>, settings: SoundSettings): Promise<void> {
+        await appSettingsHttpService.updateSoundSettings(settings);
+    },
     async loadSessionSettings(context: ActionContext<ISettingsState, any>): Promise<void> {
         context.commit('setSessionSettings', await appSettingsHttpService.getSessionSettings());
     },
