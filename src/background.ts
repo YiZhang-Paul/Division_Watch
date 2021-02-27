d4f11e3a8c8bf726a7eeee97ab80735ba1a58aac
YI ZHANG <scaccterran@gmail.com>
2021-02-27 08:50:31 -0500

    POM-36 setup preload file to include ipcRenderer

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron';
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
+import * as path from 'path';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -19,7 +20,8 @@ async function createWindow() {
         webPreferences: {
             // Use pluginOptions.nodeIntegration, leave this alone
             // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-            nodeIntegration: (process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean
+            nodeIntegration: (process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean,
+            preload: path.join(__dirname, 'preload.js')
         }
     });
 
