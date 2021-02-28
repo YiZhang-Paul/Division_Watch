872491253825e60f7c52f48209c2a424e0c5b761
YI ZHANG <scaccterran@gmail.com>
2021-02-28 12:51:38 -0500

    POM-29 show incomplete tasks and interruptions

--- src/features/daily-planner/DailyPlanner.vue
+++ src/features/daily-planner/DailyPlanner.vue
@@ -6,6 +6,22 @@
             </div>
         </template>
 
+        <div class="main-content">
+            <item-list-panel class="item-list-panel" @item:search="searchText = $event">
+                <task-summary-card class="summary-card"
+                    v-for="candidate of candidates"
+                    :key="candidate.id"
+                    :task="candidate"
+                    @mouseenter="onCardHover()">
+                </task-summary-card>
+
+                <placeholder-panel v-if="!candidates.length"
+                    class="placeholder-panel"
+                    :text="searchText ? 'no matching entry found.' : 'no entry available.'">
+                </placeholder-panel>
+            </item-list-panel>
+        </div>
+
         <template v-slot:footer>
             <div class="footer-content">
                 <menu-button class="back-button" @click="backToMain()">Back</menu-button>
@@ -19,20 +35,44 @@
 import { Options, Vue } from 'vue-class-component';
 
 import store from '../../store';
+import { soundKey } from '../../store/sound/sound.state';
 import { mainViewKey } from '../../store/main-view/main-view.state';
+import { taskItemKey } from '../../store/task-item/task-item.state';
+// eslint-disable-next-line no-unused-vars
+import { TaskItem } from '../../core/data-model/task-item/task-item';
+import { SoundOption } from '../../core/data-model/generic/sound-option';
+import TaskSummaryCard from '../../shared/cards/TaskSummaryCard.vue';
 import TitlePanel from '../../shared/panels/TitlePanel.vue';
 import ViewPanel from '../../shared/panels/ViewPanel.vue';
+import ItemListPanel from '../../shared/panels/ItemListPanel.vue';
+import PlaceholderPanel from '../../shared/panels/PlaceholderPanel.vue';
 import MenuButton from '../../shared/controls/MenuButton.vue';
 import { ViewOption } from '../../core/enums/view-option.enum';
+import { SoundType } from '../../core/enums/sound-type.enum';
 
 @Options({
     components: {
+        TaskSummaryCard,
         TitlePanel,
         ViewPanel,
+        ItemListPanel,
+        PlaceholderPanel,
         MenuButton
     }
 })
 export default class DailyPlanner extends Vue {
+    public searchText = '';
+
+    get candidates(): TaskItem[] {
+        const tasks: TaskItem[] = store.getters[`${taskItemKey}/incompleteParentTasks`];
+        const interruptions: TaskItem[] = store.getters[`${taskItemKey}/incompleteInterruptions`];
+
+        return [...tasks, ...interruptions].filter(_ => _.name.toLowerCase().includes(this.searchText));
+    }
+
+    public onCardHover(): void {
+        store.dispatch(`${soundKey}/playSound`, new SoundOption('button_hover', SoundType.UI));
+    }
 
     public backToMain(): void {
         store.commit(`${mainViewKey}/setActiveView`, ViewOption.MainMenuNoop);
@@ -45,7 +85,7 @@ export default class DailyPlanner extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.header-content, .footer-content {
+.header-content, .main-content, .footer-content {
     display: flex;
     width: 100%;
     height: 100%;
@@ -53,6 +93,35 @@ export default class DailyPlanner extends Vue {
 
 .header-content {
     align-items: flex-end;
+}
+
+.main-content {
+    $margin: 1.5%;
+    $list-width: 28%;
+
+    margin-left: $margin;
+    width: calc(100% - #{$margin} * 2);
+
+    .item-list-panel {
+        width: $list-width;
+        height: 97.5%;
+
+        .summary-card {
+            width: 100%;
+            height: 9.5vh;
+            opacity: 0;
+            animation: revealContent 0.3s ease 0.1s forwards;
+
+            &:not(:nth-last-child(1)) {
+                margin-bottom: 0.9vh;
+            }
+        }
+
+        .placeholder-panel {
+            margin-left: 2.5%;
+            width: 95%;
+        }
+    }
 }
 
 .footer-content {
